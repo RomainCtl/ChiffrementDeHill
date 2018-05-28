ded4e5ecf43ba21a0b4d7ba5e8fb23032bc3de19
Simon Le Bars <simon.lebars.22290@gmail.com>
2018-05-28 10:50:36 +0200

    ok

--- nb_cle_possibles.py
+++ nb_cle_possibles.py
@@ -5,3 +5,22 @@ Created on Fri May 25 11:58:17 2018
 @author: dell E5440
 """
 
+import numpy as np
+from values import *
+
+"""
+Programme de comptage du nombre de clés possibles 
+en fonction de la taille de la matrice
+"""
+
+def nb_cle_hill(m):
+    M = np.zeros(m)
+    
+    for i in range()
+    for x in range(len(M)):
+        for y in range(len)
+
+"""
+Max de clé possible pour un code Hill 2x2
+"""
+
